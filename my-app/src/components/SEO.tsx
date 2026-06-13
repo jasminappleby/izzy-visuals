7a23af6831497ee878d9080007781f0a6c487997
Jasmin Appleby Holdham <60107486+jasminappleby@users.noreply.github.com>
2026-06-13 19:22:08 +0000

    added scripts

---
+++ my-app/src/components/SEO.tsx
@@ -0,0 +1,54 @@
+import { Helmet } from 'react-helmet-async';
+
+interface SEOProps {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  type?: string;
+  keywords?: string;
+}
+
+export default function SEO({
+  title = 'Izzy Visuals - Professional Photography',
+  description = 'Freelance photographer specialising in sports, events & lifestyle photography. Capturing moments that matter.',
+  image = '/images/og-image.jpg',
+  url = 'https://izzyvisuals.com',
+  type = 'website',
+  keywords = 'photography, sports photography, event photography, lifestyle photography, freelance photographer'
+}: SEOProps) {
+  const fullTitle = title.includes('Izzy Visuals') ? title : `${title} | Izzy Visuals`;
+  
+  return (
+    <Helmet>
+      {/* Basic Meta Tags */}
+      <title>{fullTitle}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <meta name="author" content="Izzy" />
+      
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={fullTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:site_name" content="Izzy Visuals" />
+      
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:url" content={url} />
+      <meta name="twitter:title" content={fullTitle} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      
+      {/* Canonical */}
+      <link rel="canonical" href={url} />
+      
+      {/* Additional SEO */}
+      <meta name="robots" content="index, follow" />
+      <meta name="language" content="English" />
+      <meta name="revisit-after" content="7 days" />
+    </Helmet>
+  );
+}
